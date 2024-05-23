a6503daa535837a761f3e3788e01535ae748225e
Anton <unknown3294@gmail.com>
2024-05-23 11:25:44 +0300

    Первые сподвижки

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,14 +3,29 @@
 
 namespace CST_EMI_Shield_Wizard
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+
+        private void HelpMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            HelpWindow helpWindow = new HelpWindow();
+            helpWindow.ShowDialog();
+        }
+        private void AboutMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            AboutWindow aboutWindow = new AboutWindow();
+            aboutWindow.ShowDialog();
+        }
+        private void FeedbackMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            FeedbackWindow feedbackWindow = new FeedbackWindow();
+            feedbackWindow.ShowDialog();
         }
     }
 }
